7d6acab309407a1f609d4183d4073727ee8c6616
Hong Zhong <zh2624823227@163.com>
2017-05-11 16:41:47 +0800

    add stats about every person

--- labelShow.py
+++ labelShow.py
@@ -16,6 +16,14 @@ import cStringIO
 
 
 
+def readImage(url):
+    file = cStringIO.StringIO(urllib2.urlopen(url).read())
+    imgOri = Image.open(file)
+    img = cv2.cvtColor(np.array(imgOri),cv2.COLOR_RGB2BGR)
+    return img
+
+
+#plot only one box in a particular image
 def printBox(img,leftTop,rightBottom):
     rows = img.shape[0]
     cols = img.shape[1]
@@ -25,7 +33,7 @@ def printBox(img,leftTop,rightBottom):
     cv2.imshow('Fig',img)
     cv2.waitKey(0)
 
-
+#plot multiple boxes in a image
 def printBoxInSameImage(img,leftTopList,rightBottomList,i):
     rows = img.shape[0]
     cols = img.shape[1]
@@ -40,33 +48,6 @@ def printBoxInSameImage(img,leftTopList,rightBottomList,i):
     
     
 
-
-def printBoxInSameImage2(img,leftTopList,rightBottomList,i,num,figNum):
-    assert( len(leftTopList)==len(rightBottomList) )
-    for ix in range(len(leftTopList)):
-        leftTop = leftTopList[ix]
-        rightBottom = rightBottomList[ix]
-        cv2.rectangle(img,leftTop,rightBottom,(255,0,255),3)
-        
-    (r, g, b)=cv2.split(img)  
-    img=cv2.merge([b,g,r])  
-    #figName = 'Fig'+str(i)
-    plt.subplot(1,figNum,i+1)
-    plt.xticks([],[])#do not show ticks
-    plt.yticks([],[])
-    plt.title('User'+str(num))
-    plt.imshow(img)
-
-
-
-
-def readImage(url):
-    file = cStringIO.StringIO(urllib2.urlopen(url).read())
-    imgOri = Image.open(file)
-    img = cv2.cvtColor(np.array(imgOri),cv2.COLOR_RGB2BGR)
-    return img
-
-
 def processOneImage(index):
 #    index = 100 
     imageName = label.ix[index]['ImageName']
@@ -78,6 +59,7 @@ def processOneImage(index):
     topleft = (int(pos['topleft']['x']*cols),int(pos['topleft']['y']*rows))
     bottomright = (int(pos['bottomright']['x']*cols),int(pos['bottomright']['y']*rows))
     printBox(img,topleft,bottomright)
+    
     
 
 def processSameImage(index,i):
@@ -116,6 +98,30 @@ def processSameImage(index,i):
 
 
 
+
+# plot box in image
+def printBoxInSameImage2(img,leftTopList,rightBottomList,i,num,figNum):
+    assert( len(leftTopList)==len(rightBottomList) )
+    for ix in range(len(leftTopList)):
+        leftTop = leftTopList[ix]
+        rightBottom = rightBottomList[ix]
+        cv2.rectangle(img,leftTop,rightBottom,(255,0,255),3)
+        
+    (r, g, b)=cv2.split(img)  
+    img=cv2.merge([b,g,r])  
+    #figName = 'Fig'+str(i)
+    plt.subplot(1,figNum,i+1)
+    plt.xticks([],[])#do not show ticks
+    plt.yticks([],[])
+    plt.title('User'+str(num))
+    plt.imshow(img)
+
+
+# process image with the same imageID
+# index: the row index of image in dataFrame
+# i: for plotting index
+# num : the user
+# figNum : the total figure to be plotted
 def processSameImage2(index,i,num,figNum):
     startIndex = index
     stopIndex = index
@@ -153,13 +159,10 @@ def processSameImage2(index,i,num,figNum):
 
 
 
-
-
-
+#find Last Image With particular ImageID And UserID, return the Row Index of labels(dataFrame) 
 def findLastImageWithImageIDAndUserID(targetImageID,targetUserID,indexList):
     index = 0
     while index < len(imageIDList):
-        #print index,
         if imageIDList[index] == targetImageID and userIDList[index] == targetUserID:
             while imageIDList[index] == targetImageID and userIDList[index] == targetUserID and index < len(imageIDList)-1 :
                 index = index+1
@@ -175,7 +178,7 @@ def showImageWithNumK(i):
     indexList = []
     for userIDPar in userIDSet:
         findLastImageWithImageIDAndUserID(targetImageID,userIDPar,indexList)
-      
+     
     for i in range(len(indexList)):
         processSameImage(indexList[i],i)
     cv2.waitKey(0)
@@ -189,8 +192,8 @@ def showImageWithNumK2(i):
     
     #print indexList
     for i in range(len(indexList)):
-        emailAdd = label.ix[indexList[i]]['Email']
-        num = mapEmailToUserID(emailAdd)
+        emailAddress = label.ix[indexList[i]]['Email']
+        num = mapEmailToUserID(emailAddress)
         processSameImage2(indexList[i],i,num,len(indexList))
     plt.show()
     
@@ -205,6 +208,73 @@ def mapEmailToUserID(email):
     else:
         pass
 
+
+def statsAboutPerson():
+    #userDict = dict.fromkeys(('user1','user2','user3'),0)
+    #stats: user 1
+    totalFig = 0
+    totalBox = 0
+    totalArea = 0.0
+    userImageIDSet=set()
+    for index in range(label.shape[0]):
+        if label.ix[index]['Email'] == 'user1@cc.com':
+            userImageIDSet.add(label.ix[index]['ImageID'])
+            pos = label.ix[index]['PointsJson']
+            pos = eval(pos)
+            area = abs(pos['topleft']['x']-pos['bottomright']['x'])*abs(pos['topleft']['y']-pos['bottomright']['y'])
+            totalArea += area
+            totalBox += 1
+    totalFig = len(userImageIDSet)
+    print "user1: totalFig: %d, totalBox: %d, totalArea: %f"%(totalFig,totalBox,totalArea)
+    print "user1: averageBox: %f, averageArea: %f"%(totalBox*1.0/totalFig,totalArea*1.0/totalFig)
+    print
+    #stats: user 2
+    totalFig = 0
+    totalBox = 0
+    totalArea = 0.0
+    userImageIDSet=set()
+    for index in range(label.shape[0]):
+        if label.ix[index]['Email'] == 'user2@cc.com':
+            userImageIDSet.add(label.ix[index]['ImageID'])
+            pos = label.ix[index]['PointsJson']
+            pos = eval(pos)
+            area = abs(pos['topleft']['x']-pos['bottomright']['x'])*abs(pos['topleft']['y']-pos['bottomright']['y'])
+            totalArea += area
+            totalBox += 1
+    totalFig = len(userImageIDSet)
+    print "user2: totalFig: %d, totalBox: %d, totalArea: %f"%(totalFig,totalBox,totalArea)
+    print "user2: averageBox: %f, averageArea: %f"%(totalBox*1.0/totalFig,totalArea*1.0/totalFig)
+    print
+    #stats: user 3
+    totalFig = 0
+    totalBox = 0
+    totalArea = 0.0
+    userImageIDSet=set()
+    for index in range(label.shape[0]):
+        if label.ix[index]['Email'] == 'user3@cc.com':
+            userImageIDSet.add(label.ix[index]['ImageID'])
+            pos = label.ix[index]['PointsJson']
+            pos = eval(pos)
+            area = abs(pos['topleft']['x']-pos['bottomright']['x'])*abs(pos['topleft']['y']-pos['bottomright']['y'])
+            totalArea += area
+            totalBox += 1
+    totalFig = len(userImageIDSet)
+    print "user3: totalFig: %d, totalBox: %d, totalArea: %f"%(totalFig,totalBox,totalArea)
+    print "user3: averageBox: %f, averageArea: %f"%(totalBox*1.0/totalFig,totalArea*1.0/totalFig)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+            
+        
+
+
       
 
 if __name__ == '__main__': 
@@ -213,18 +283,19 @@ if __name__ == '__main__':
     imageIDList = list(label['ImageID'])
     userIDSet = set(label['UserID'])
     userIDList = list(label['UserID'])
+    emailSet = set(label['Email'])
     
     userIDEmailSet = set()
     for i in range(label.shape[0]):
         userIDEmailSet.add((label.ix[i]['UserID'],label.ix[i]['Email']))
     
-    userIDEmailSet = set()
-    for i in range(label.shape[0]):
-        userIDEmailSet.add((label.ix[i]['UserID'],label.ix[i]['Email']))
-        
+    '''  
     num = raw_input("Select Image Num from 0 to %d:\n"%(len(imageIDSet)-1))
     num = int(num)
     showImageWithNumK2(num)
+    '''
+    
+    statsAboutPerson()
     
     
     
